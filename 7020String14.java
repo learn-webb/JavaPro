48354148587ab7c1d99ff134f20a441f25587eeb
learn-webb <107128369+learn-webb@users.noreply.github.com>
2023-11-08 13:33:04 +0530

    Add fileString14

---
+++ 7020String14.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class CountCharToString{
+public static void main(String a[])
+{
+
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter a String:");
+String str=sc.nextLine();
+int charactercount=str.length();
+System.out.println("Total number of character in a string:"+charactercount);
+}
+}
+
